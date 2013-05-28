96232d842e6e7c34a2eef05ce0162ba44feda5a3
Aditya Eka <tyo1811@yahoo.com.sg>
2013-05-28 13:07:00 +0700

    New file for problem 2 solving

---
+++ problem2solving.php
@@ -0,0 +1,37 @@
+<?php
+
+class FizzAndBuzz
+{
+	public static function run($fromNum, $loopUntilNumber)
+	{
+		$prev1 = "";
+		$prev2 = "";
+		for($x=$fromNum; $x<=$loopUntilNumber; $x++){
+			$t = "";
+			if ($x % 3 == 0){
+				$t = "Fizz" ; 
+			}
+			if ($x % 5 == 0){
+				$t = "Buzz"; 
+			}
+			
+			if ($t == ""){
+				if ($prev1 != "" && $prev2 != ""){
+					echo "Bazz";
+				}else{
+					echo $x;
+				}
+			}else if ($t != ""){
+				echo $t;
+			}
+			$prev2 = ($prev1!=""?$prev1:"");
+                        $prev1 = ($t!=""?$t:"");
+			echo " ";
+		}
+		echo "\n";
+	}
+}
+
+// Test
+echo "Running FizzBuzzBazz with range 4 to 20:\n";
+FizzAndBuzz::run(4,20);
